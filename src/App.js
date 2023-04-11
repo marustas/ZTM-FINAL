0e40ae56debdd3e1e25621cc04ce3178aafd9762
Stanislau Marudau <marudov.stas@gmail.com>
2023-04-11 15:14:32 +0300

    Basic components added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
-import Navigation from './components/Navigation';
+import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+ <ParticlesBg type="cobweb" bg={true} />
   <Navigation />
-  {<Logo/>
-  /*
+  <Logo />
+  <Rank />
   <ImageLinkForm />
+  {
+  /*
     <FaceRecognition />*/}
       </div>
     );
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Rank = () =>{
+return(
+<div>
+    <div className = "white f3">
+      {'Stas, your current rank is ...'}
+    <div className = "white f1">
+      {'#5'}
+    </div>
+    </div>
+</div>
+ );
+}
+
+export default Rank;
