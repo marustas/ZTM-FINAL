209653352232e755d79dab76a0d67c372e070ae4
Stanislau Marudau <marudov.stas@gmail.com>
2023-05-07 23:01:28 +0300

    API implementation

--- src/App.js
+++ src/App.js
@@ -5,22 +5,33 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+import Clarifai from 'clarifai';
 
-
+const app = new Clarifai.App({
+  apiKey : '3bc66df5e5604cefb0e79231ac135cb0'
+});
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      input: '3bc66df5e5604cefb0e79231ac135cb0',
+      input: '',
     }
   }
   onInputChange = (event) => {
     console.log(event.target.value);
 }
+onSubmit = () =>{
+  console.log('click');
+  app.models.predict("6dc7e46bc9124c5c8824be4822abe105", "https://samples.clarifai.com/metro-north.jpg").then(
+    function(response){
+console.log(response);
+    },
+    function(err){
 
-  onSubmit =() =>{
-console.log('click');
-  }
+    }
+  );
+}
+
   render() {
     return (
       <div className="App">
@@ -29,6 +40,7 @@ console.log('click');
   <Logo />
   <Rank />
   <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onSubmit} />
+
   {
   /*
     <FaceRecognition />*/}
