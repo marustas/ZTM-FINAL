1684f063f02e3751803b4bfea783bff7acf8d520
Stanislau Marudau <marudov.stas@gmail.com>
2023-05-11 00:17:08 +0300

    Image box download and center enabled

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 
 const returnClarifaiRequestOptions =(imageUrl) =>{
 // Your PAT (Personal Access Token) can be found in the portal under Authentification
 const PAT = '52b1b44f603643f898fa75627bd79517';
 // Specify the correct user_id/app_id pairings
-// Since you're making inferences outside your app's scope
 const USER_ID = 'stasstone2004';       
 const APP_ID = 'brain';
 // Change these to whatever model and image URL you want to use
@@ -49,46 +49,32 @@ class App extends Component {
     super()
     this.state = {
       input: '',
+      imageUrl: ''
     }
   }
 
   onSubmit =() =>{
-console.log('click');
-fetch("https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs", returnClarifaiRequestOptions())
+    this.setState({imageUrl: this.state.input})
+    console.log('click');
+    fetch("https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs", returnClarifaiRequestOptions(this.state.input))
     .then(response => response.json())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+    .then(result => console.log(result.outputs[0].data.regions[0].region_info.bounding_box))
 
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value})
 }
-/*
-onSubmit = () =>{
-  console.log('click');
-  app.models.predict("6dc7e46bc9124c5c8824be4822abe105", "https://samples.clarifai.com/metro-north.jpg").then(
-    function(response){
-console.log(response);
-    },
-    function(err){
-    }
-  );
-}
-*/
 
   render() {
     return (
       <div className="App">
- <ParticlesBg type="cobweb" bg={true}/>
+  <ParticlesBg type="cobweb" bg={true}/>
   <Navigation/>
   <Logo />
   <Rank />
   <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onSubmit} />
-
-  {
-  /*
-    <FaceRecognition />*/}
+  <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
