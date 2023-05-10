a0ca0085912956f31b9b000dd243a8c2c680c0ba
Stanislau Marudau <marudov.stas@gmail.com>
2023-05-10 21:59:47 +0300

    API implementation completed

--- src/App.js
+++ src/App.js
@@ -5,11 +5,45 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
-import Clarifai from 'clarifai';
 
-const app = new Clarifai.App({
-  apiKey : '3bc66df5e5604cefb0e79231ac135cb0'
+const returnClarifaiRequestOptions =(imageUrl) =>{
+// Your PAT (Personal Access Token) can be found in the portal under Authentification
+const PAT = '52b1b44f603643f898fa75627bd79517';
+// Specify the correct user_id/app_id pairings
+// Since you're making inferences outside your app's scope
+const USER_ID = 'stasstone2004';       
+const APP_ID = 'brain';
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = 'face-detection'; 
+const IMAGE_URL = imageUrl;
+const raw = JSON.stringify({
+  "user_app_id": {
+      "user_id": USER_ID,
+      "app_id": APP_ID
+  },
+  "inputs": [
+      {
+          "data": {
+              "image": {
+                  "url": IMAGE_URL
+              }
+          }
+      }
+  ]
 });
+
+const requestOptions = {
+  method: 'POST',
+  headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+  },
+  body: raw
+};
+
+return requestOptions;
+}
+
 class App extends Component {
   constructor(){
     super()
@@ -17,9 +51,20 @@ class App extends Component {
       input: '',
     }
   }
+
+  onSubmit =() =>{
+console.log('click');
+fetch("https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs", returnClarifaiRequestOptions())
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+  }
+
   onInputChange = (event) => {
     console.log(event.target.value);
 }
+/*
 onSubmit = () =>{
   console.log('click');
   app.models.predict("6dc7e46bc9124c5c8824be4822abe105", "https://samples.clarifai.com/metro-north.jpg").then(
@@ -27,10 +72,10 @@ onSubmit = () =>{
 console.log(response);
     },
     function(err){
-
     }
   );
 }
+*/
 
   render() {
     return (
