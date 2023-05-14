b38aaecf4caee98a60eb4866b440240f443c5321
Stanislau Marudau <marudov.stas@gmail.com>
2023-05-14 16:11:53 +0300

    Sign in form added and styled

--- src/App.js
+++ src/App.js
@@ -1,103 +1,107 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ParticlesBg from 'particles-bg';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn';
 import './App.css';
 
-const returnClarifaiRequestOptions =(imageUrl) =>{
-// Your PAT (Personal Access Token) can be found in the portal under Authentification
-const PAT = '52b1b44f603643f898fa75627bd79517';
-// Specify the correct user_id/app_id pairings
-const USER_ID = 'stasstone2004';       
-const APP_ID = 'brain';
-// Change these to whatever model and image URL you want to use
-const MODEL_ID = 'face-detection'; 
-const IMAGE_URL = imageUrl;
-const raw = JSON.stringify({
-  "user_app_id": {
-      "user_id": USER_ID,
-      "app_id": APP_ID
-  },
-  "inputs": [
-      {
-          "data": {
-              "image": {
-                  "url": IMAGE_URL
-              }
-          }
-      }
-  ]
-});
+const returnClarifaiRequestOptions = (imageUrl) => {
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = '52b1b44f603643f898fa75627bd79517';
+    // Specify the correct user_id/app_id pairings
+    const USER_ID = 'stasstone2004';
+    const APP_ID = 'brain';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'face-detection';
+    const IMAGE_URL = imageUrl;
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [{
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }]
+    });
 
-const requestOptions = {
-  method: 'POST',
-  headers: {
-      'Accept': 'application/json',
-      'Authorization': 'Key ' + PAT
-  },
-  body: raw
-};
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
 
-return requestOptions;
+    return requestOptions;
 }
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {}
+    constructor() {
+        super()
+        this.state = {
+            input: '',
+            imageUrl: '',
+            box: {},
+            route: 'signin'
+        }
     }
-  }
 
-  calculateFaceLocation=(data)=>{
-const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-const image = document.getElementById('inputimage');
-const width = Number(image.width);
-const height = Number(image.height);
-return{
-  leftCol: clarifaiFace.left_col * width,
-  rightCol: width - (clarifaiFace.right_col * width),
-  topRow: clarifaiFace.top_row * height,
-  bottomRow: height - (clarifaiFace.bottom_row * height)
-}
-}
+    calculateFaceLocation = (data) => {
+        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+        const image = document.getElementById('inputimage');
+        const width = Number(image.width);
+        const height = Number(image.height);
+        return {
+            leftCol: clarifaiFace.left_col * width,
+            rightCol: width - (clarifaiFace.right_col * width),
+            topRow: clarifaiFace.top_row * height,
+            bottomRow: height - (clarifaiFace.bottom_row * height)
+        }
+    }
 
-  onSubmit =() =>{
-    this.setState({imageUrl: this.state.input})
-    console.log('click');
-    // eslint-disable-next-line no-useless-concat
-    fetch("https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs", returnClarifaiRequestOptions(this.state.input))
-    .then(response => response.json())
-    .then(result => this.displayFaceBox(this.calculateFaceLocation(result)))
-    .catch(err => console.log(err))
-  }
+    onSubmit = () => {
+        this.setState({ imageUrl: this.state.input })
+        console.log('click');
+        // eslint-disable-next-line no-useless-concat
+        fetch("https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs", returnClarifaiRequestOptions(this.state.input))
+            .then(response => response.json())
+            .then(result => this.displayFaceBox(this.calculateFaceLocation(result)))
+            .catch(err => console.log(err))
+    }
 
-  displayFaceBox =(box) =>{
-    console.log(box);
-    this.setState({box: box});
-  }
+    displayFaceBox = (box) => {
+        console.log(box);
+        this.setState({ box: box });
+    }
 
-  onInputChange = (event) => {
-   this.setState({input: event.target.value})
-}
+    onInputChange = (event) => {
+        this.setState({ input: event.target.value })
+    }
 
-  render() {
-    return (
-      <div className="App">
-  <ParticlesBg type="cobweb" bg={true}/>
-  <Navigation/>
-  <Logo />
-  <Rank />
-  <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onSubmit} />
-  <FaceRecognition box ={this.state.box} imageUrl={this.state.imageUrl} />
-      </div>
-    );
-  }
+    render() {
+        return ( 
+        <div className = "App" >
+            <ParticlesBg type = "cobweb" bg = { true }/> 
+            <Navigation/>
+          {this.state.route === 'signin'? <SignIn/> : 
+          <div>
+            <Logo/>
+            <Rank/>
+            <ImageLinkForm onInputChange = { this.onInputChange } onButtonSubmit = { this.onSubmit }/> 
+            <FaceRecognition box = { this.state.box } imageUrl = { this.state.imageUrl }/>
+          </div>  
+          }
+        </div>
+        );
+    }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,24 +1,17 @@
 import React from "react";
 import './ImageLinkForm.css'
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
-    return ( <
-        div >
-        <
-        p className = "f3" > { 'This Magic Brain detects faces' } <
-        /p> <
-        div className = "Center" >
-        <
-        div className = "Form Center pa4 br3 shadow-5" >
-        <
-        input className = "f4 pa2 w-70 center"
+    return ( 
+    <div >
+        <p className = "f3" > { 'This Magic Brain detects faces' } </p> <div className = "Center" >
+        <div className = "Form Center pa4 br3 shadow-5" >
+        <input className = "f4 pa2 w-70 center"
         type = 'tex'
-        onChange = { onInputChange }
-        /> <
-        button className = "w-30 grow f4 link ph3 pv dib white bg-light-purple"
-        onClick = { onButtonSubmit } > Detect < /button> <
-        /div> <
-        /div> <
-        /div>
+        onChange = { onInputChange }/>
+         <button className = "w-30 grow f4 link ph3 pv dib white bg-light-purple" onClick = { onButtonSubmit } > Detect </button> 
+         </div> 
+         </div> 
+         </div>
     );
 }
 
