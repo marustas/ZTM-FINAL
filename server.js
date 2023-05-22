44768280ce05e5813fc51237750739347f023bd4
Stanislau Marudau <marudov.stas@gmail.com>
2023-05-22 17:05:49 +0300

    Documentation added

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('working as expected');
+})
+
 app.listen(3000, () => {
     console.log('all good');
 })
+
+/*
+/ --> res = this is not working
+/signin --> POST = successful
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> POST --> user
+*/
