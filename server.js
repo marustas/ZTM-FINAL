eda3cfa4ab3f5fa594911ab4017923bbb5d2fdfb
Stanislau Marudau <marudov.stas@gmail.com>
2023-05-25 19:36:28 +0300

    BodyParser and signin component congigured

--- server.js
+++ server.js
@@ -2,12 +2,45 @@ const express = require('express');
 
 const app = express();
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+const database = {
+    users: [{
+            id: '123',
+            gmail: 'ponus@gmail.com',
+            password: 'wrongpassword',
+            name: 'Asmanus',
+            surname: 'Ramazanus',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            gmail: 'moschneishiy@gmail.com',
+            password: 'rightpassword',
+            name: 'Stasus',
+            surname: 'Yussufus',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
 app.get('/', (req, res) => {
     res.send('working as expected');
 })
 
 app.post('/signin', (req, res) => {
-    res.send('sign in')
+    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+        res.json('success');
+    } else {
+        res.status(400).json('error logging in');
+    }
+})
+
+app.post('/register', (req, res) => {
+    res.json('sign in')
 })
 
 app.listen(3000, () => {
@@ -16,7 +49,7 @@ app.listen(3000, () => {
 
 /*
 / --> res = this is not working
-/signin --> POST = successful
+/signin --> POST = successful --done
 /register --> POST = user
 /profile/:userId --> GET = user
 /image --> POST --> user
