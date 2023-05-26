c5ec5d16e26119033970fc56d8acbb76ad6ab243
Stanislau Marudau <marudov.stas@gmail.com>
2023-05-26 21:57:01 +0300

    Divided the code itnto server and frontend directories

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ class App extends Component {
     }
 
     componentDidMount(){
-        fetch('http://localhost:3000')
+        fetch('http://localhost:3000/')
         .then(response=>response.json)
         .then(console.log)//same as data =>console.log(data)
     }
