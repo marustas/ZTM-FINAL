1b2759b1880ab38e58ad7479faa04429bcdcd13f
Stanislau Marudau <marudov.stas@gmail.com>
2023-05-26 17:50:41 +0300

    Server connection set up

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const app = express();
 
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(cors);
 
 const database = {
     users: [{
