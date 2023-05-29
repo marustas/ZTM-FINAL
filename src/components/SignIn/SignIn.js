a6ba986688a64696dae545af9d0a5b41a5d8d6dd
Stanislau Marudau <marudov.stas@gmail.com>
2023-05-29 15:29:38 +0300

    Component naming fixed

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Signin extends React.Component {
+class SignIn extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -82,4 +82,4 @@ class Signin extends React.Component {
   }
 }
 
-export default Signin;
+export default SignIn;
